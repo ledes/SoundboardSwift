a4bd79d647dfbaf5c84b81d16ea9ff55a50c1939
Pablo <pablo.ledesma.p@gmail.com>
2016-01-10 12:09:46 -0500

    Add table and cells

---
+++ Soundboard/SoundListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SoundListViewController.swift
+//  Soundboard
+//
+//  Created by Pablo on 1/10/16.
+//  Copyright © 2016 Pablo Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel!.text = "bla"
+        return cell
+    }
+}
+
